041eeb98094829b6a7ddc534bfe1866d3a4ee2cf
haosizheng <shibi@MacBook-Pro-4.local>
2023-05-18 23:24:34 +0200

    Updated: Add Monument basic

--- Assets/Scripts/FireworkManager.cs
+++ Assets/Scripts/FireworkManager.cs
@@ -5,6 +5,8 @@ using Unity.Netcode;
 
 public class FireworkManager : MonoBehaviour
 {
+    public Vector3 TargetPosition = new(0,0,0);
+
     [SerializeField] GameObject m_FirePrefab;
 
     [SerializeField] Vector3 m_Offset = new(0f, 0f, 2f);
@@ -14,7 +16,16 @@ public class FireworkManager : MonoBehaviour
         var camera = Camera.main;
         Debug.Log($"Camera position: {camera.transform.position}");
         Debug.Log($"Camera rotation: {camera.transform.rotation}");
-        var firework = Instantiate(m_FirePrefab, camera.transform.position + camera.transform.rotation * m_Offset, camera.transform.rotation);
+
+        //var fixedPosition = camera.transform.position + camera.transform.rotation * m_Offset;
+        var fixedPosition = TargetPosition + RandomVector3(-1f,1f);
+
+        var firework = Instantiate(m_FirePrefab, fixedPosition, camera.transform.rotation);
         firework.GetComponent<NetworkObject>().Spawn();
+    }
+
+    Vector3 RandomVector3(float min, float max)
+    {
+        return new Vector3(Random.Range(min, max), Random.Range(min, max), Random.Range(min, max));
     }
 }
