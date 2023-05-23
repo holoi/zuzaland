27a44c2b8fa129910377e281c99ac798ca1f9645
haosizheng <shibi@MacBook-Pro-4.local>
2023-05-23 15:22:26 +0200

    Updated: Fixed camera distance of fov

--- Assets/Scripts/Monument.cs
+++ Assets/Scripts/Monument.cs
@@ -15,23 +15,23 @@ public class Monument : MonoBehaviour
     void Update()
     {
 
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-        {
+        //if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        //{
 
-            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
-            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
-            //Select Hool
-            if (hit && hit.collider.transform.name == "Monument")
-            {
-                if (!IsSelected)
-                {
-                    IsSelected = true;
-                }
-                else
-                {
-                    IsSelected = false;
-                }
-            }
-        }
+        //    Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+        //    RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+        //    //Select Hool
+        //    if (hit && hit.collider.transform.name == "Monument")
+        //    {
+        //        if (!IsSelected)
+        //        {
+        //            IsSelected = true;
+        //        }
+        //        else
+        //        {
+        //            IsSelected = false;
+        //        }
+        //    }
+        //}
     }
 }
